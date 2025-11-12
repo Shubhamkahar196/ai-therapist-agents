d1295a21ece93056891062e68dc079f84ecd940d
Shubham kahar <Shubhamkahar196@gmail.com>
2025-11-12 21:11:54 +0530

    Added Footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Header } from "./components/header";
 import { Providers } from "./components/provider";
+import { Footer } from "./components/footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -33,6 +34,7 @@ export default function RootLayout({
           {/* header */}
           <Header />
           {children}
+          <Footer/>
         </Providers>
       </body>
     </html>
