b4a010ad5e47a4f1f42a6caa7ed9ab05a686a8e5
Shubham kahar <Shubhamkahar196@gmail.com>
2025-11-14 19:14:08 +0530

    Added signup and login

--- app/components/provider.tsx
+++ app/components/provider.tsx
@@ -1,16 +1,19 @@
 'use client'
 
 import { ThemeProvider } from "next-themes"
+import { SessionProvider } from "@/lib/contexts/session-context"
 
 export function Providers({children}: {children : React.ReactNode}) {
     return (
-        <ThemeProvider 
+        <ThemeProvider
         attribute="class"
         defaultTheme="system"
         enableSystem
         disableTransitionOnChange
         >
-            {children}
+            <SessionProvider>
+                {children}
+            </SessionProvider>
         </ThemeProvider>
     )
 }
