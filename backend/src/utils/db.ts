2e0bb44c34b69002aabf6adee2ec4e0c0b6d1336
Shubham kahar <Shubhamkahar196@gmail.com>
2025-11-29 09:40:58 +0530

    Completed backend and frontend

--- backend/src/utils/db.ts
+++ backend/src/utils/db.ts
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 import { logger } from "./logger.js";
 
-const MONGODB_URI = process.env.MONGODB_URI as string;
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb+srv://yt727883:wTg5f7j1Ju3y0iQN@cluster0.1tsxm.mongodb.net/ai-therapist";
 
 if (!MONGODB_URI) {
   throw new Error(" MONGODB_URI is missing in .env file");
